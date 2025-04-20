1dca94a6f89b1f031ff3fc7e4a6b98470a33598d
Yui Nishimura <nisshi.yui79@gmail.com>
2025-04-20 15:34:32 +0000

    some ovserv files

---
+++ include/test.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "myMalloc.h"
+
+#include <setjmp.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
---
+++ test/originalMallocObserv.c
@@ -0,0 +1,7 @@
+#include "test.h"
+
+int main(int argc, char *argv[])
+{
+    char *ptr = malloc(100);
+    
+}
